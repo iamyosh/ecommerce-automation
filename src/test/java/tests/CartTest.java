4e07cc820aff5cca33f09031e6af82e62fa937a4
iamyosh <yoshedirisinghe28@gmail.com>
2026-01-07 12:44:02 +0530

    all page edits and additions in the cart test

--- src/test/java/tests/CartTest.java
+++ src/test/java/tests/CartTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
 import pages.Productspage;
 import pages.SignupFormpage;
 import pages.Signuppage;
@@ -10,4 +11,18 @@ public class CartTest extends BaseTest{
     private Signuppage signuppage;
     private SignupFormpage signupFormpage;
     private Productspage productspage;
+
+    @Test
+    public void testCart(){
+        signuppage = homepage.goToSignup();
+        signuppage.setSignupDetails("Yosh", "fjf@gmail.com");
+
+        signupFormpage = signuppage.goToSignupForm();
+        signupFormpage.fillSignupForm();
+
+        productspage = signupFormpage.backToProducts();
+        productspage.scroll();
+        productspage.addProductsToCart();;
+        productspage.goToCart();
+    }
 }
