f3b86dda0d320026287979da024ebc99220dcd26
iamyosh <yoshedirisinghe28@gmail.com>
2026-01-07 12:39:45 +0530

    signup and signupForm edits in cart

--- src/test/java/tests/CartTest.java
+++ src/test/java/tests/CartTest.java
@@ -1,7 +1,11 @@
 package tests;
 
 import org.openqa.selenium.WebDriver;
+import pages.SignupFormpage;
+import pages.Signuppage;
 
 public class CartTest extends BaseTest{
     private WebDriver driver;
+    private Signuppage signuppage;
+    private SignupFormpage signupFormpage;
 }
