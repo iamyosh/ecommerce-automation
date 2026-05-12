f4084428862643fc233c3a58b7e04d3e80cf0d03
iamyosh <yoshedirisinghe28@gmail.com>
2026-05-12 16:23:40 +0530

    automatic screenshot after method runs

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,7 +1,17 @@
 package tests;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 import pages.Loginpage;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 public class LoginTest extends BaseTest {
 
@@ -9,5 +19,23 @@ public class LoginTest extends BaseTest {
     public void loginTest(){
         Loginpage loginpage = new Loginpage(driver);
         loginpage.setLogin("test@example.com", "123456");
+    }
+
+    @AfterMethod
+    public void takeScreenshot(){
+        TakesScreenshot camera = (TakesScreenshot) driver;
+        File screenshot = camera.getScreenshotAs(OutputType.FILE);
+
+        try {
+            Files.createDirectories(Paths.get("resources/screenshots"));
+            Files.move(
+                    screenshot.toPath(),
+                    Paths.get("resources/screenshots/failed-login.png"),
+                    StandardCopyOption.REPLACE_EXISTING
+            );
+        } catch (IOException exception) {
+            exception.printStackTrace();
+         }
+
     }
 }
